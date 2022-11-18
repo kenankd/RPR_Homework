fe5ff6d20d043ad4d34b4e5afcaf9e7ab8e50f60
Kenan <kdizdarevi1@etf.unsa.ba>
2022-11-18 01:48:45 +0100

    finalna verzija 1.0

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,19 +1,33 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Objects;
+
+import static ba.unsa.etf.rpr.ExpressionCheck.*;
+
 /**
- * Hello world!
+ * Class that tests ExpressionEvaluator, gets expression through command line
  *
  */
-public class App 
-{
-    public static void main( String[] args ){
-    try{
-        ExpressionEvaluator e=new ExpressionEvaluator();
-        Double rezultat = e.evaluate("( 1 + ( ( 2 + 3 ) * ) 4 * 5 ( ) )");
-        System.out.println("Rezultat aritmetickog izraza je " + rezultat);
-    }
-    catch(RuntimeException e){
-        System.out.println("Aritmeticki izraz nije validan");
-    }
+public class App {
+    public static void main(String[] args) {
+        try {
+            if (args.length < 4 || !Objects.equals(args[0], "(")) {
+                throw new RuntimeException();
+            }
+            StringBuilder build = new StringBuilder("");
+            for (String s : args) {
+                if (!isOperator(s) && !isOperand(s) && !isOperatorSqrt(s) && !s.equals("(") && !s.equals(")"))
+                    throw new RuntimeException();
+                build.append(s).append(" ");
+            }
+            ExpressionEvaluator e = new ExpressionEvaluator();
+            String rez=build.toString();
+            Double result = e.evaluate(rez.trim());
+            System.out.println(result);
+        }
+        catch(RuntimeException e){
+            System.out.println("Izraz nije ispravan");
+        }
+
     }
 }
